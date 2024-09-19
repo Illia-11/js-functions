4f834183b46835279a4f5e5425b08ac691e803a5
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-19 20:51:17 +0300

    Add new version of project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,55 @@
 // task1
 console.log('=== Task 1 ===');
-const singleDigitNum = +prompt('Enter a number: ');
-if (singleDigitNum < 10 && singleDigitNum >= 0) {
-  console.log('Right number!');
-} else {
-  console.log('False number!');
+const getSmallerNumber = function() {
+  const number1 = prompt('Enter first number: ');
+  const number2 = prompt('Enter second number: ');
+  if(typeof number1 !== 'number' || typeof number2 !== 'number') {
+    return null;
+  }
+  // if(number1 > number2) {
+  //   return number2;
+  // } else {
+  //   return number1;
+  // }
+  return number1 > number2 ? number2 : number1;
 }
+console.log(getSmallerNumber());
 
 // task2
 console.log('=== Task 2 ===');
-const age = +prompt('Enter your age: ');
-if (age >= 18) {
-  console.log('You are an adult!');
-} else {
-  console.log('You are a child!');
+const isAdult = function() {
+  const age = prompt('Enter you age: ');
+  // if(age >= 18) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return age >=18 ? true : false;
 }
+console.log(isAdult());
 
 // task3
 console.log('=== Task 3 ===');
-const sumPrice = +prompt('Enter a sum: ');
-if (sumPrice >= 1500) {
-  console.log(sumPrice - (sumPrice / 100 * 10));
-} else {
-  console.log(sumPrice - (sumPrice / 100 * 5));
+const price = function() {
+  const Price = prompt('Enter Sum: ');
+  // if(Price >= 1500) {
+  //   return Price - Price / 100 * 10;
+  // } else {
+  //   return Price - Price / 100 * 5;
+  // }
+  return Price >= 1500 ? Price - Price / 100 * 10 :  Price - Price / 100 * 5;
 }
+console.log(price());
 
 // task4
-console.log('=== Task 4 ===');
-const evenNumber = +prompt('Enter a number: ');
-if (evenNumber % 2 === 0) {
-  console.log('Number is even!');
-} else {
-  console.log('Number is odd!');
+console.log('=== Task 3 ===');
+const isEven = function() {
+  const Number = prompt('Enter a number: ');
+  // if(Number % 2 === 0) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return Number % 2 === 0 ? true : false;
 }
+console.log(isEven());
