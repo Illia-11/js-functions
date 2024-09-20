08161e23406036a8ea1e4378d9ca341627d4ad92
Illia Piddubnyi <illya2008zp@gmail.com>
2024-09-20 11:44:56 +0300

    Add new version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 // task1
 console.log('=== Task 1 ===');
-const getSmallerNumber = function() {
-  const number1 = prompt('Enter first number: ');
-  const number2 = prompt('Enter second number: ');
+const getSmallerNumber = function(number1, number2) {
   if(typeof number1 !== 'number' || typeof number2 !== 'number') {
+    return null;
+  }
+  if (isNaN(number1)) {
+    return null;
+  }
+  if (isNaN(number2)) {
     return null;
   }
   // if(number1 > number2) {
@@ -13,13 +17,18 @@ const getSmallerNumber = function() {
   // }
   return number1 > number2 ? number2 : number1;
 }
-console.log(getSmallerNumber());
+const number1 = +prompt('Enter first number');
+const number2 = +prompt('Enter second number');
+console.log(getSmallerNumber(number1, number2));
+
 
 // task2
 console.log('=== Task 2 ===');
-const isAdult = function() {
-  const age = prompt('Enter you age: ');
+const isAdult = function(age) {
   if(typeof age !== 'number') {
+    return null;
+  }
+  if (isNaN(age)) {
     return null;
   }
   // if(age >= 18) {
@@ -29,29 +38,36 @@ const isAdult = function() {
   // }
   return age >=18 ? true : false;
 }
-console.log(isAdult());
+const age = +prompt('Enter you age: ');
+console.log(isAdult(age));
+
 
 // task3
 console.log('=== Task 3 ===');
-const price = function() {
-  const Price = prompt('Enter Sum: ');
-  if(typeof Price !== 'number') {
+const price = function(sale) {
+  if(typeof sale !== 'number') {
     return null;
   }
-  // if(Price >= 1500) {
-  //   return Price - Price / 100 * 10;
+  if (isNaN(sale)) {
+    return null;
+  }
+  // if(sale >= 1500) {
+  //   return sale - sale / 100 * 10;
   // } else {
-  //   return Price - Price / 100 * 5;
+  //   return sale - sale / 100 * 5;
   // }
-  return Price >= 1500 ? Price - Price / 100 * 10 :  Price - Price / 100 * 5;
+  return sale >= 1500 ? sale - sale / 100 * 10 :  sale - sale / 100 * 5;
 }
-console.log(price());
+const sale = +prompt('Enter Sum: ');
+console.log(price(sale));
 
 // task4
-console.log('=== Task 3 ===');
-const isEven = function() {
-  const Number = prompt('Enter a number: ');
-  if(typeof Number !== 'number') {
+console.log('=== Task 4 ===');
+const isEven = function(evNumber) {
+  if(typeof evNumber !== 'number') {
+    return null;
+  }
+  if (isNaN(evNumber)) {
     return null;
   }
   // if(Number % 2 === 0) {
@@ -59,6 +75,7 @@ const isEven = function() {
   // } else {
   //   return false;
   // }
-  return Number % 2 === 0 ? true : false;
+  return evNumber % 2 === 0 ? true : false;
 }
-console.log(isEven());
+const evNumber = +prompt('Enter a number: ');
+console.log(isEven(evNumber));
